e3bac7309b422b4e73542265d706e538ba8fb8a9
NeelsK <nkriek@me.com>
2025-10-23 11:14:19 +0200

    Update mac-mic-control.sh

--- mac-mic-control.sh
+++ mac-mic-control.sh
@@ -7,10 +7,13 @@
 TARGET_VOLUME=5
 
 while true; do
-  CURRENT_VOLUME=$(osascript -e "input volume of (get volume settings)")
-  if [ "$CURRENT_VOLUME" -ne "$TARGET_VOLUME" ]; then
-    osascript -e "set volume input volume $TARGET_VOLUME"
-    echo "Volume reset to $TARGET_VOLUME% at $(date)"
+  CURRENT_INPUT=$(SwitchAudioSource -t input -c)
+  if [[ "$CURRENT_INPUT" == "AirPods Max" ]]; then
+    CURRENT_VOLUME=$(osascript -e "input volume of (get volume settings)")
+    if [ "$CURRENT_VOLUME" -ne "$TARGET_VOLUME" ]; then
+      osascript -e "set volume input volume $TARGET_VOLUME"
+      echo "Volume reset to $TARGET_VOLUME% at $(date)"
+    fi
+    sleep 3
   fi
-  sleep 3
 done
